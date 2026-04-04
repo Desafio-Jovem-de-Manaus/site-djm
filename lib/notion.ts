609827cc000378c69be8b8fde78368790a030e6b
Desafio Jovem de Manaus <joaovitor@desafiojovemmanaus.org.br>
2026-04-04 16:08:31 -0400

    fix: corrigir filtro de status do Notion

--- lib/notion.ts
+++ lib/notion.ts
@@ -12,7 +12,7 @@ export async function getPublishedPosts() {
     filter: {
       property: "Status",
       status: {
-        equals: "Concluída",
+        equals: "Concluído",
       },
     },
     sorts: [
